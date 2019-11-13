17c07c510a6b0fc579214a66b18d304770d2fdbc
aphiratnimanussonkul <b5923151@gmail.com>
2019-11-13 09:53:58 +0700

    edit get book's name

--- main.go
+++ main.go
@@ -13,9 +13,12 @@ import (
 func main() {
 	//var url = "http://reg3.sut.ac.th/registrar/calendar.asp?schedulegroupid=101&acadyear=2562&semester=1"
 	// var url = "https://www.amazon.com/Animal-Farm-GEORGE-ORWELL/dp/9386538288/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1573527970&sr=8-1"
-	var url = "https://www.amazon.com/Animal-Farm-Large-George-Orwell-dp-4871872696/dp/4871872696/ref=mt_paperback?_encoding=UTF8&me=&qid=1573546394"
+	// var url = "https://www.amazon.com/Animal-Farm-Large-George-Orwell-dp-4871872696/dp/4871872696/ref=mt_paperback?_encoding=UTF8&me=&qid=1573546394"
 	// var url = "https://www.amazon.com/Harraps-Slovene-Phrasebook/dp/0071546111/ref=sr_1_1?crid=10BCROLSA6WY3&keywords=harraps+book&qid=1573550166&s=books&sprefix=harra%2Cstripbooks-intl-ship%2C388&sr=1-1"
 	// var url = "https://www.amazon.com/Dasd-Direct-Access-Storage-Devices/dp/0070326746/ref=sr_1_1?keywords=dasd&qid=1573554307&s=books&sr=1-1"
+	// var url = "https://www.amazon.com/gp/product/1558329919/ref=s9_acsd_al_bw_c_x_3_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-8&pf_rd_r=WND1J9YNKCC0099P8GVX&pf_rd_t=101&pf_rd_p=69e92e00-dd37-4469-9e77-9c4b5c15c5a4&pf_rd_i=283155"
+	var url = "https://www.amazon.com/Wrecking-Ball-Diary-Wimpy-Book/dp/1419739034/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=&sr="
+	// var url = "https://www.amazon.com/If-You-Tell-Unbreakable-Sisterhood/dp/1542005221/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=&sr="
 	doc, err := Init(url)
 	if err == nil {
 		imageUrl, err := GetUrlImgage(doc)
@@ -65,12 +68,12 @@ func GetBookName(doc *goquery.Document) (reulst string, err error) {
 	var book_name string
 	// var firstTime bool = true
 	doc.Find("h1#title").Each(func(i int, s *goquery.Selection) {
-		band := s.Find("span").Text()
+		band := s.Find("span#productTitle").Text()
 		// fmt.Println(i, band)
-		if i == 0 {
-			book_name = band
-			return
-		}
+		// if i == 0 {
+		book_name = band
+		return
+		// }
 
 	})
 	return book_name, nil
